e9140a261b960ad1e739a08fa80785c889f812c0
Sudarshan patil <sdrshnptl@users.noreply.github.com>
2020-11-01 19:01:05 +0530

    added printf

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <cstdio>
 
+
 Ticker timer_ms;
 
 uint16_t ovf_cnt_1ms = 0;
@@ -18,7 +19,8 @@ uint8_t tx_buff_len = 0, tx_buff[32];
 DigitalOut led(LED1);
 AnalogIn   ain(A0);
 
- static BufferedSerial serial_port(USBTX, USBRX);
+BufferedSerial serial_port(USBTX, USBRX);
+
 
 void timer_1ms()
 {
@@ -28,13 +30,6 @@ void timer_1ms()
 
 int main()
 {
-    printf("%f\r\n", MBED_BUILD_TIMESTAMP);
-    serial_port.set_baud(9600);
-    serial_port.set_format(
-        /* bits */ 8,
-        /* parity */ BufferedSerial::None,
-        /* stop bit */ 1
-    );
 
     timer_ms.attach(timer_1ms, 1ms);
     
@@ -46,7 +41,6 @@ int main()
             ovf_cnt_1ms = 0;
             led = !led; 
 
-            //serial_port.write("Hello\r\n", 7);
             printf("Ain: %d \r\n", (uint8_t) ain.read_u16());
         }
     }
